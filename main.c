8ae8223f34556716326f57259323df4d33a8a7fc
Alaux <73968015+MrAlaux@users.noreply.github.com>
2023-12-21 00:45:09 -0300

    Rewrite

--- main.c
+++ main.c
@@ -1,101 +1,159 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-#define PARMCOUNT 6
+typedef enum { false, true } boolean;
 
-float min[PARMCOUNT], max[PARMCOUNT];
 
-float random(int index)
+#define MAXPARMS 8
+
+
+typedef struct {
+  char *description;
+  char *prototype;
+  int numparms;
+  enum { T_INT, T_FLOAT, T_STRING } const parmtype[MAXPARMS];
+} func_t;
+
+func_t *func;
+
+typedef enum {
+  FUNC_SPAWNOBJECT,
+  FUNC_MONSTERPROJECTILE,
+
+  NUMFUNCS
+} funcs_t;
+
+func_t funcs[NUMFUNCS] = {
+  {
+    "A_SpawnObject(type, angle, x_ofs, y_ofs, z_ofs, x_vel, y_vel, z_vel)",
+    "A_SpawnObject(%s, %s, %s, %s, %s, %s, %s, %s)",
+    8, { T_STRING, T_FLOAT,  T_FLOAT,  T_FLOAT,  T_FLOAT,  T_FLOAT,  T_FLOAT,  T_FLOAT  }
+  },
+
+  {
+    "A_MonsterProjectile(type, angle, pitch, hoffset, voffset)",
+    "A_MonsterProjectile(%s, %s, %s, %s, %s)%s%s%s",
+    5, { T_STRING, T_FLOAT,  T_FLOAT,  T_FLOAT,  T_FLOAT  }
+  },
+};
+
+
+float min[MAXPARMS], max[MAXPARMS];
+
+// Taken from StackOverflow
+float frandom(int index)
 {
-  return min[index] + (rand() / (float) RAND_MAX) * (max[index] - min[index]);
+  return min[index] + ((rand() / (float) RAND_MAX) * (max[index] - min[index]));
 }
+
+char *parameters[MAXPARMS][BUFSIZ];
+
+char *random(int index)
+{
+  char *const parameter = parameters[index];
+
+  if (func->numparms <= index) {
+    snprintf(parameter, BUFSIZ, "");
+  }
+  else switch (func->parmtype[index])
+  {
+    case T_INT:
+      snprintf(parameter, BUFSIZ, "%i", (int) round(frandom(index)));
+      break;
+
+    case T_FLOAT:
+      snprintf(parameter, BUFSIZ, "%0.1f", frandom(index));
+      break;
+
+    case T_STRING:
+      snprintf(parameter, BUFSIZ, "PARM%i", index);
+      break;
+  }
+
+  return parameter;
+}
+
 
 int main()
 {
-  FILE *fptr;
-  int val, parms;
   int i;
+  int numstates, func_index;
+  FILE *fptr;
 
-  for (i = 0;  i < PARMCOUNT;  i++) { min[i] = max[i] = 0.0; }
+  for (i = 0;  i < MAXPARMS;  i++) { min[i] = max[i] = 0.0f; }
 
   srand(time(NULL));
 
-  printf("Enter val (int): ");
-  scanf("%i", &val);
-  printf("\n");
+  printf("Enter number of states [int]: ");
+  scanf("%i", &numstates);
+  puts("");
 
-  printf("Parameters (0 = All, 1 = Ofs, 2 = Vel): ");
-  scanf("%i", &parms);
-  printf("\n");
+  printf("Available functions:\n");
+  for (i = 0;  i < NUMFUNCS;  i++)
+  { printf("[%i] %s\n", i, funcs[i].description); }
+  puts("");
 
-  parms = (parms < 0 ? 0 :
-           parms > 2 ? 2 : parms);
+  printf("Select function [int]: ");
+  scanf("%i", &func_index);
+  puts("");
 
-  if (parms == 2) { i = 3; }
-  else            { i = 0; }
+  func = &funcs[func_index];
 
-  for (;  i < PARMCOUNT;  i++)
+  for (i = 0;  i < func->numparms;  i++)
   {
-    if (i == 3 && parms == 1) { break; }
+    if (func->parmtype[i] == T_STRING) { continue; }
 
-    printf("Enter min%i (float): ", i+1);
+    printf("Enter min%i [float]: ", i + 1);
     scanf("%f", &min[i]);
 
-    printf("Enter max%i (float): ", i+1);
+    printf("Enter max%i [float]: ", i + 1);
     scanf("%f", &max[i]);
 
-    printf("\n");
+    puts("");
   }
 
   fptr = fopen("./sd21-randomizer.txt", "w");
 
   if (!fptr) {
-    printf("ERROR opening file.\n");   
+    puts("ERROR opening file.");   
     exit(1);             
   }
-  else for (i = 1;  i <= val;  i++)
-  {
-    char buffer[BUFSIZ];
+  else {
+    char format[BUFSIZ];
 
-    if (parms == 2)
-    {
-      snprintf(buffer, BUFSIZ,
-               "    Spawn%i:\n"
-               "      TNT1 A 1 A_SpawnObject(thing, 0.0, 0.0, 0.0, 0.0, %0.1f, %0.1f, %0.1f)\n"
-               "      Stop\n",
-               i, random(3), random(4), random(5)
-              );
-    }
-    else if (parms == 1)
-    {
-      snprintf(buffer, BUFSIZ,
-               "    Spawn%i:\n"
-               "      TNT1 A 1 A_SpawnObject(thing, 0.0, %0.1f, %0.1f, %0.1f)\n"
-               "      Stop\n",
-               i, random(0), random(1), random(2)
-              );
-    }
-    else
-    {
-      snprintf(buffer, BUFSIZ,
-               "    Spawn%i:\n"
-               "      TNT1 A 1 A_SpawnObject(thing, 0.0, %0.1f, %0.1f, %0.1f, %0.1f, %0.1f, %0.1f)\n"
-               "      Stop\n",
-               i, random(0), random(1), random(2), random(3), random(4), random(5)
-              );
-    }
+    snprintf(
+      format, BUFSIZ,
+      "    Spawn%%i:\n"
+      "      TNT1 A 1 %s\n"
+      "      Stop\n",
+      func->prototype
+    );
 
-    printf("%s", buffer);
-    fprintf(fptr, "%s", buffer);
+    for (i = 1;  i <= numstates;  i++)
+    {
+      char buffer[BUFSIZ];
+
+      snprintf(
+        buffer, BUFSIZ,
+        format,
+        i, random(0), random(1), random(2), random(3), random(4), random(5), random(6), random(7)
+      );
+
+      printf("%s", buffer);
+      fprintf(fptr, "%s", buffer);
+    }
   }
 
   fclose(fptr);
 
-  printf("\n"
-         "Operation successful. Text file written.\n"
-         "Please hit Enter to exit.");
+  printf(
+    "\n"
+    "Operation successful. Text file written.\n"
+    "Please hit Enter to exit."
+  );
 
   while (getchar() != '\n');
   while (getchar() != '\n');
