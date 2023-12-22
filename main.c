a37fde6096c2edeffd52fc257805003b04533e28
Alaux <73968015+MrAlaux@users.noreply.github.com>
2023-12-21 21:55:19 -0300

    Tweaks
    - Const correctness (?)
    - Separated function name into its own struct member
    - Correct `parameters` type
    - Implemented quick test

--- main.c
+++ main.c
@@ -11,13 +11,14 @@ typedef enum { false, true } boolean;
 
 
 typedef struct {
-  char *description;
-  char *prototype;
-  int numparms;
-  enum { T_INT, T_FLOAT, T_STRING } const parmtype[MAXPARMS];
+  const char name[32];
+  const char description[80];
+  const char prototype[40];
+  const int  numparms;
+  const enum { T_INT, T_FLOAT, T_STRING } parmtype[MAXPARMS];
 } func_t;
 
-func_t *func;
+const func_t *func;
 
 typedef enum {
   FUNC_SPAWNOBJECT,
@@ -26,16 +27,18 @@ typedef enum {
   NUMFUNCS
 } funcs_t;
 
-func_t funcs[NUMFUNCS] = {
+const func_t funcs[NUMFUNCS] = {
   {
-    "A_SpawnObject(type, angle, x_ofs, y_ofs, z_ofs, x_vel, y_vel, z_vel)",
-    "A_SpawnObject(%s, %s, %s, %s, %s, %s, %s, %s)",
+    "A_SpawnObject",
+    "type, angle, x_ofs, y_ofs, z_ofs, x_vel, y_vel, z_vel",
+    "(%s, %s, %s, %s, %s, %s, %s, %s)",
     8, { T_STRING, T_FLOAT,  T_FLOAT,  T_FLOAT,  T_FLOAT,  T_FLOAT,  T_FLOAT,  T_FLOAT  }
   },
 
   {
-    "A_MonsterProjectile(type, angle, pitch, hoffset, voffset)",
-    "A_MonsterProjectile(%s, %s, %s, %s, %s)%s%s%s",
+    "A_MonsterProjectile",
+    "type, angle, pitch, hoffset, voffset",
+    "(%s, %s, %s, %s, %s)%s%s%s",
     5, { T_STRING, T_FLOAT,  T_FLOAT,  T_FLOAT,  T_FLOAT  }
   },
 };
@@ -49,7 +52,7 @@ float frandom(int index)
   return min[index] + ((rand() / (float) RAND_MAX) * (max[index] - min[index]));
 }
 
-char *parameters[MAXPARMS][BUFSIZ];
+char parameters[MAXPARMS][BUFSIZ];
 
 char *random(int index)
 {
@@ -69,7 +72,7 @@ char *random(int index)
       break;
 
     case T_STRING:
-      snprintf(parameter, BUFSIZ, "PARM%i", index);
+      snprintf(parameter, BUFSIZ, "PARM%i", index + 1);
       break;
   }
 
@@ -83,21 +86,33 @@ int main()
   int numstates, func_index;
   FILE *fptr;
 
+  #if 1
+  #define TEST
+  #endif
+
   for (i = 0;  i < MAXPARMS;  i++) { min[i] = max[i] = 0.0f; }
 
   srand(time(NULL));
 
   printf("Enter number of states [int]: ");
+  #ifndef TEST
   scanf("%i", &numstates);
+  #else
+  printf("%i\n", numstates = 16);
+  #endif
   puts("");
 
   printf("Available functions:\n");
   for (i = 0;  i < NUMFUNCS;  i++)
-  { printf("[%i] %s\n", i, funcs[i].description); }
+  { printf("[%i] %s(%s)\n", i, funcs[i].name, funcs[i].description); }
   puts("");
 
   printf("Select function [int]: ");
+  #ifndef TEST
   scanf("%i", &func_index);
+  #else
+  printf("%i\n", func_index = 0);
+  #endif
   puts("");
 
   func = &funcs[func_index];
@@ -107,10 +122,18 @@ int main()
     if (func->parmtype[i] == T_STRING) { continue; }
 
     printf("Enter min%i [float]: ", i + 1);
+    #ifndef TEST
     scanf("%f", &min[i]);
+    #else
+    printf("%f\n", min[i] = i + 1.0);
+    #endif
 
     printf("Enter max%i [float]: ", i + 1);
+    #ifndef TEST
     scanf("%f", &max[i]);
+    #else
+    printf("%f\n", max[i] = i + 2.0);
+    #endif
 
     puts("");
   }
@@ -127,9 +150,9 @@ int main()
     snprintf(
       format, BUFSIZ,
       "    Spawn%%i:\n"
-      "      TNT1 A 1 %s\n"
+      "      TNT1 A 1 %s%s\n"
       "      Stop\n",
-      func->prototype
+      func->name, func->prototype
     );
 
     for (i = 1;  i <= numstates;  i++)
@@ -155,7 +178,9 @@ int main()
     "Please hit Enter to exit."
   );
 
+  #ifndef TEST
   while (getchar() != '\n');
+  #endif
   while (getchar() != '\n');
 
   return 0;
