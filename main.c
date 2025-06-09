88fb1f517d40f60407fc633756e2b365dfc4f0cc
Alaux <73968015+MrAlaux@users.noreply.github.com>
2025-06-09 19:57:05 -0300

    Various changes
    Notable ones (not all):
    - Made `Get_String()` receive the string instead of allocating it itself
    - Use standard `bool` instead of custom `boolean`
    - Replaced some literal numbers with declared constants
    - Refactoring and reformatting
    - Updated copyright year and added notice to code file

--- main.c
+++ main.c
@@ -1,108 +1,129 @@
+//
+// Copyright (C) 2023-2025 Alaux (MIT License)
+//
+
 #include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-typedef enum { false, true } boolean;
-
-boolean Get_String(char **getter)
+bool Get_String(char *const getter, const size_t size)
 {
-  const size_t MAX_CHARS = 64;
-  char input[MAX_CHARS];
+  memset(getter, '\0', size);
 
-  fgets(input, MAX_CHARS, stdin);
+  fgets(getter, size, stdin);
 
-  if (!(strlen(input) - 1)) { return true; } // Don't count the newline
+  if (getter[0] == '\0')
+  {
+    // 'ETX' received
+    putchar('\n');
+  }
+  else { getter[strcspn(getter, "\n")] = '\0'; }
 
-  *getter = strdup(input);
-  return false;
+  if (getter[size - 2] != '\0')
+  {
+    // Excess characters received; discard said excess
+    for (char c; (c = getchar()) != '\n' && c != EOF;);
+  }
+
+  return !strlen(getter);
 }
 
-boolean Get_Int(int *getter)
+bool Get_Int(int *const getter)
 {
-  char *input;
+  const size_t MAX_LENGTH = 12; // "-2147483648" has 11 characters; add one for the null terminator
+  char input[MAX_LENGTH];
 
-  if (Get_String(&input)) { goto error1; }
+  if (Get_String(input, MAX_LENGTH)) { return true; }
 
-  const size_t input_size = strlen(input) - 1; // Don't count the newline
+  const size_t input_size = strlen(input);
 
-  boolean num   = false,
-          minus = false;
+  bool num   = false,
+       minus = false;
 
   for (int i = 0;  i < input_size;  i++)
   {
     const char c = input[i];
 
     if ('0' <= c && c <= '9')
-    { num = true; }
+    {
+      num = true;
+    }
     else if (c == '-' && !minus && !i)
-    { minus = true; }
-    else
-    { goto error2; }
+    {
+      minus = true;
+    }
+    else { return true; }
   }
 
-  if (!num) { goto error2; }
+  if (!num) { return true; }
 
   *getter = atoi(input);
-  free(input);
   return false;
-
-  error2: free(input);
-  error1: return true;
 }
 
-boolean Get_Float(float *getter)
+bool Get_Float(float *const getter)
 {
-  char *input;
+  const size_t MAX_LENGTH = 33; // Not exact, but reasonable: 32 chars plus null terminator
+  char input[MAX_LENGTH];
 
-  if (Get_String(&input)) { goto error1; }
+  if (Get_String(input, MAX_LENGTH)) { return true; }
 
-  const size_t input_size = strlen(input) - 1; // Don't count the newline
+  const size_t input_size = strlen(input);
 
-  boolean num   = false,
-          dot   = false,
-          minus = false;
+  bool num   = false,
+       dot   = false,
+       minus = false;
 
   for (int i = 0;  i < input_size;  i++)
   {
     const char c = input[i];
 
     if ('0' <= c && c <= '9')
-    { num = true; }
+    {
+      num = true;
+    }
     else if (c == '.' && !dot)
-    { dot = true; }
+    {
+      dot = true;
+    }
     else if (c == '-' && !minus && !i)
-    { minus = true; }
-    else
-    { goto error2; }
+    {
+      minus = true;
+    }
+    else { return true; }
   }
 
-  if (!num) { goto error2; }
+  if (!num) { return true; }
 
   *getter = atof(input);
-  free(input);
   return false;
-
-  error2: free(input);
-  error1: return true;
 }
 
+
+typedef enum type_e {
+  T_INT,
+  T_FLOAT,
+  T_STRING,
+
+  NUM_TYPES
+} type_t;
 
 #define MAX_PARMS 8
 
-
 typedef struct {
-  const char name[32];
-  const char description[80];
-  const char prototype[40];
-  const int  num_parms;
-  const enum { T_INT, T_FLOAT, T_STRING } parm_type[MAX_PARMS];
+  const char    name[32];
+  const char    description[80];
+  const char    prototype[40];
+  const int     num_parms;
+  const type_t  parm_type[MAX_PARMS];
 } func_t;
 
 const func_t *func;
 
-typedef enum {
+typedef enum funcs_e {
   FUNC_SPAWNOBJECT,
   FUNC_MONSTERPROJECTILE,
 
@@ -116,7 +137,6 @@ const func_t funcs[NUM_FUNCS] = {
     "(%s, %s, %s, %s, %s, %s, %s, %s)",
     8, { T_STRING, T_FLOAT,  T_FLOAT,  T_FLOAT,  T_FLOAT,  T_FLOAT,  T_FLOAT,  T_FLOAT  }
   },
-
   {
     "A_MonsterProjectile",
     "type, angle, pitch, hoffset, voffset",
@@ -133,28 +153,29 @@ float Random_Float(int index)
   return min[index] + (((double) rand() / RAND_MAX) * (max[index] - min[index]));
 }
 
-char parameters[MAX_PARMS][32];
-
 char *Random(int index)
 {
+  #define PARAMETER_SIZE 32
+  static char parameters[MAX_PARMS][PARAMETER_SIZE];
+
   char *const parameter = parameters[index];
 
   if (func->num_parms <= index)
   {
-    snprintf(parameter, 32, "");
+    snprintf(parameter, PARAMETER_SIZE, "");
   }
   else switch (func->parm_type[index])
   {
     case T_INT:
-      snprintf(parameter, 32, "%i", (int) round(Random_Float(index)));
+      snprintf(parameter, PARAMETER_SIZE, "%i", (int) round(Random_Float(index)));
       break;
 
     case T_FLOAT:
-      snprintf(parameter, 32, "%0.1f", Random_Float(index));
+      snprintf(parameter, PARAMETER_SIZE, "%0.1f", Random_Float(index));
       break;
 
     case T_STRING:
-      snprintf(parameter, 32, "PARM%i", index + 1);
+      snprintf(parameter, PARAMETER_SIZE, "PARM%i", index + 1);
       break;
   }
 
@@ -164,22 +185,23 @@ char *Random(int index)
 
 int main()
 {
-  #if 0
+#if 0
   #define TEST
-  #endif
+#endif
 
-  FILE *fptr = fopen("./sd21-randomizer.txt", "w");
+  #define PATH "./sd21-randomizer.txt"
 
-  if (!fptr) {
-    puts("ERROR opening output file.");
-    exit(1);
+  FILE *const file = fopen(PATH, "w");
+
+  if (!file) {
+    puts("ERROR opening \"" PATH "\".");
+    return 1;
   }
 
   srand(time(NULL));
 
-  int i;
-
-  for (i = 0;  i < MAX_PARMS;  i++) { min[i] = max[i] = 0.0f; }
+  memset(min, 0, sizeof(*min) * MAX_PARMS);
+  memset(max, 0, sizeof(*max) * MAX_PARMS);
 
   int num_states;
 
@@ -187,20 +209,20 @@ int main()
   {
     printf("Enter number of states [int]: ");
 
-    #ifndef TEST
+  #ifndef TEST
     if (!Get_Int(&num_states) && num_states > 0)
     { break; }
-    #else
+  #else
     printf("%i\n", num_states = 16);
     break;
-    #endif
+  #endif
   }
 
   puts("");
 
   printf("Available functions:\n");
 
-  for (i = 0;  i < NUM_FUNCS;  i++)
+  for (int i = 0;  i < NUM_FUNCS;  i++)
   { printf("[%i] %s(%s)\n", i, funcs[i].name, funcs[i].description); }
 
   puts("");
@@ -211,20 +233,20 @@ int main()
   {
     printf("Select function [int]: ");
 
-    #ifndef TEST
+  #ifndef TEST
     if (!Get_Int(&func_index) && (0 <= func_index && func_index < NUM_FUNCS))
     { break; }
-    #else
+  #else
     printf("%i\n", func_index = 0);
     break;
-    #endif
+  #endif
   }
 
   puts("");
 
   func = &funcs[func_index];
 
-  for (i = 0;  i < func->num_parms;  i++)
+  for (int i = 0;  i < func->num_parms;  i++)
   {
     if (func->parm_type[i] == T_STRING) { continue; }
 
@@ -232,61 +254,63 @@ int main()
     {
       printf("Enter min%i [float]: ", i + 1);
 
-      #ifndef TEST
+    #ifndef TEST
       if (!Get_Float(&min[i]))
       { break; }
-      #else
+    #else
       printf("%f\n", min[i] = i + 1.0);
       break;
-      #endif
+    #endif
     }
 
     while (true)
     {
       printf("Enter max%i [float]: ", i + 1);
 
-      #ifndef TEST
+    #ifndef TEST
       if (!Get_Float(&max[i]))
       { break; }
-      #else
+    #else
       printf("%f\n", max[i] = i + 2.0);
       break;
-      #endif
+    #endif
     }
 
     puts("");
   }
 
-  char format[256];
+  #define FORMAT_SIZE 256
+  char format[FORMAT_SIZE];
 
   snprintf(
-    format, 256,
+    format, FORMAT_SIZE,
     "    Spawn%%i:\n"
     "      TNT1 A 1 %s%s\n"
     "      stop\n",
     func->name, func->prototype
   );
 
-  for (i = 1;  i <= num_states;  i++)
+  for (int i = 1;  i <= num_states;  i++)
   {
-    char buffer[512];
+    #define BUFFER_SIZE 512
+    char buffer[BUFFER_SIZE];
 
     snprintf(
-      buffer, 512,
+      buffer, BUFFER_SIZE,
       format,
       i, Random(0), Random(1), Random(2), Random(3), Random(4), Random(5), Random(6), Random(7)
     );
 
     printf("%s", buffer);
-    fprintf(fptr, "%s", buffer);
+    fprintf(file, "%s", buffer);
   }
 
-  fclose(fptr);
+  fclose(file);
 
   printf(
     "\n"
-    "Operation successful. Text file written.\n"
-    "Please hit Enter to exit."
+    "Operation successful: \"" PATH "\" written.\n"
+    "Press Enter to exit."
   );
 
   while (getchar() != '\n');
